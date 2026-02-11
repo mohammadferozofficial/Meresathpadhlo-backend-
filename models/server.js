bf37971644124cc61c2cdb6f4a841ff03a8fd372
Mohammed Feroz <mohammedferoz.fffs@gmail.com>
2026-02-11 13:46:41 +0530

    CORS fixed properly

--- models/server.js
+++ models/server.js
@@ -1,3 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
+
 
